69da7cbd64baa4d467ccea55fb9ff92f28c8c967
Jesse <JGarcia.x@icloud.com>
2019-08-28 11:55:18 +1000

    work in progress

--- client/client.c
+++ client/client.c
@@ -6,20 +6,50 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
-
+#include <libgen.h>
+#define BUFF_SIZE 1024
 #define PORT 8080
+
+void sendfile(FILE *fp, int sockfd){
+    int n, total = 0;
+    char sendline[BUFF_SIZE] = {0};
+    while ((n = fread(sendline, sizeof(char), BUFF_SIZE, fp)) > 0)
+    {
+        total+=n;
+        if (n != BUFF_SIZE && ferror(fp))
+        {
+            perror("Read File Error");
+            exit(1);
+        }
+
+        if (send(sockfd, sendline, n, 0) == -1)
+        {
+            perror("Can't send file");
+            exit(1);
+        }
+        memset(sendline, 0, BUFF_SIZE);
+    }
+}
+
+
+
+
+
 
 int main(){
 
-	int client_sock = 0, valread ;
-    	struct sockaddr_in serv_addr;
-    	char buffer[1024] = {0};
+	int client_sock = 0, valread, total = 0 ;
+	struct sockaddr_in serv_addr;
+    char buffer[BUFF_SIZE] = {0};
+    char filename[BUFF_SIZE] = {0};
+    char *p_buffer = buffer;
+    char *token[BUFF_SIZE];
 
 	//If there is an error creating the socket
-    	if ((client_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
-        	printf("\n [-]Socket creation error \n");
-        	return -1;
-    	}
+	if ((client_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+	    printf("\n [-]Socket creation error \n");
+	    return -1;
+	}
 
 	printf("[+] Client Socket is made.\n");
 
@@ -43,12 +73,12 @@ int main(){
 	while(1){
 		printf("Please enter the message: \t");
 		bzero(buffer,sizeof(buffer));
-	      	fgets(buffer, 1024, stdin);
-	      	valread = write(client_sock, buffer, strlen(buffer));
+		fgets(buffer, BUFF_SIZE, stdin);
+		valread = write(client_sock, buffer, strlen(buffer));
 
-	      	if(valread < 0){
-	        	perror("Error taking in writing");
-	      	}
+		if(valread < 0){
+		    perror("Error taking in writing");
+		}
 
 		if(strcmp(buffer, ":exit") == 0){
 			close(client_sock);
@@ -56,14 +86,52 @@ int main(){
 			exit(1);
 		}
 
-	      	//send(sock, buffer, strlen(buffer), 0);
-	      	bzero(buffer,sizeof(buffer));
-	      	valread = read(client_sock, buffer, 1024);
+        for(int i = 0; i < strlen(p_buffer); i++) {
+            token[i] = strtok_r(p_buffer, " ", &p_buffer);
+            int length = strlen(token[i]);
+            if (token[i][length-1] == '\n' || token[i][length-1] == '\t')
+                token[i][length-1]  = '\0';
+        }
 
-	      	if(valread < 0){
-	        	perror("Error reading from socket");
-	      	}
-	      	printf("Server: %s", buffer);
+//        char *filename = token[1];
+//        if (filename == NULL){
+//            perror("Can't get filename");
+//            exit(1);
+//        }
+
+//        strncpy(filename, token[1], strlen(filename));
+//        if (send(client_sock, filename, BUFF_SIZE, 0) == -1){
+//            perror("Can't send filename");
+//            exit(1);
+//        }
+//
+        FILE *fp = fopen(token[1], "a");
+        if (fp == NULL){
+            perror("Can't open file");
+            exit(1);
+        }
+
+
+        int ch = getc(fp); //Get initial input
+        while(ch != EOF){
+            putchar(ch);    //Process input
+            ch = getc(fp);
+        }
+
+        sendfile(ch, client_sock);
+        //puts("Send Success");
+        printf("Send Success, NumBytes = %ld\n", total);
+        fclose(fp);
+        close(client_sock);
+
+		//send(sock, buffer, strlen(buffer), 0);
+		bzero(buffer,sizeof(buffer));
+		valread = read(client_sock, buffer, BUFF_SIZE);
+
+		if(valread < 0){
+		    perror("Error reading from socket");
+		}
+		printf("Server: %s", buffer);
 	}
 	return 0;
 }
--- server/server.c
+++ server/server.c
@@ -7,101 +7,130 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-
 #define PORT 8080
+#define BUFF_SIZE 1024
+
+
 
 /* This function allows for the transfer of
  * files to the server from the client.
  */
-//void send_recieve_file(){
-//	while (1) {
-//		printf("\nWaiting for file name...\n");
+//void send_file(char *prog_name, char *location, int sockfd){
+//    printf("This function is working %s %s \n", prog_name, location);
+//    if (send(sockfd, prog_name, BUFF_SIZE, 0) == -1)
+//    {
+//        perror("Can't send filename");
+//        exit(1);
+//    }
 //
-//		// receive file name
-//		bzero(buffer, sizeof(buffer)); //Clear buffer
-//        	read(new_socket, buffer, 1024);
+//    FILE *fp = fopen(argv[1], "rb");
+//    if (fp == NULL)
+//    {
+//        perror("Can't open file");
+//        exit(1);
+//    }
 //
-//		nBytes = recvfrom(sockfd, net_buf, NET_BUF_SIZE, sendrecvflag,
-//				(struct sockaddr*)&addr_con, &addrlen);
-//
-//        	fp = fopen(net_buf, "r");
-//        	printf("\nFile Name Received: %s\n", net_buf);
-//        	if (fp == NULL)
-//            	printf("\nFile open failed!\n");
-//        els
-//            	printf("\nFile Successfully opened!\n");
-//
-//        	while(1){
-//
-//            		// process
-//            		if (sendFile(fp, net_buf, NET_BUF_SIZE)) {
-//                		sendto(sockfd, net_buf,
-//					NET_BUF_SIZE, sendrecvflag,
-//					(struct sockaddr*)&addr_con, addrlen);
-//                	break;
-//            	}
-//
-//            	// send
-//            	sendto(sockfd, net_buf, NET_BUF_SIZE, sendrecvflag,
-//				(struct sockaddr*)&addr_con, addrlen);
-//            	clearBuf(net_buf);
-//        	}
-//        	if (fp != NULL)
-//            	fclose(fp);
-//    	}
+//    sendfile(fp, sockfd);
+//    //puts("Send Success");
+//    printf("Send Success, NumBytes = %ld\n", total);
+//    fclose(fp);
+//    close(sockfd);
 //}
-//
+void writefile(int sockfd, FILE *fp){
+    ssize_t n;
+    int total = 0;
+    char buff[BUFF_SIZE] = {0};
+
+    while ((n = recv(sockfd, buff, BUFF_SIZE, 0)) > 0){
+        total+=n;
+        if (n == -1){
+            perror("Receive File Error");
+            exit(1);
+        }
+
+        if (fwrite(buff, sizeof(char), n, fp) != n){
+            perror("Write File Error");
+            exit(1);
+        }
+        memset(buff, 0, BUFF_SIZE);
+    }
+}
+
+void receive_file(char *prog_name, char *location, int sockfd, char *sin_addr){
+    ssize_t total = 0;
+
+    if (recv(sockfd, prog_name, BUFF_SIZE, 0) == -1){
+        perror("Can't receive filename");
+        exit(1);
+    }
+
+    FILE *fp = fopen(prog_name, "w");
+    if (fp == NULL){
+        perror("Can't open file");
+        exit(1);
+    }
+
+    char addr[INET_ADDRSTRLEN];
+    printf("Start receive file: %s from %s\n", prog_name, sin_addr);
+    writefile(sockfd, fp);
+    printf("Receive Success, NumBytes = %ld\n", total);
+
+
+    fclose(fp);
+}
+
+
 
 int main(){
 
  	struct sockaddr_in address;
 	int server_fd, new_socket, valread, opt = 1;
  	int addrlen = sizeof(address);
-  char buffer[1024] = {0};
-  char break_down[1024] = {0};
+    char buffer[BUFF_SIZE] = {0};
 	char *p_buffer = buffer;
-	char *token;
+	char *token[BUFF_SIZE];
 
 	pid_t childpid; //Data type stands for process id.
 	socklen_t addr_size; //Socklen_t is an unsigned opaque integral type length of at least 32 bits.
 
-    	// Creating socket file descriptor
-    	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0){
+	// Creating socket file descriptor
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0){
         	perror("[-]Socket failed");
         	exit(EXIT_FAILURE);
-    	}
+	}
 
 	printf("[+]Server Socket is created.\n");
 
-    	// Forcefully attaching socket to the port 8080
-    	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-                                                  &opt, sizeof(opt))){
+	// Forcefully attaching socket to the port 8080
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))){
         	perror("[-]Setsockopt");
         	exit(EXIT_FAILURE);
-    	}
-    	address.sin_family = AF_INET;
-    	address.sin_addr.s_addr = INADDR_ANY;
-    	address.sin_port = htons( PORT );
+	}
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons( PORT );
 
-    	// Forcefully attaching socket to the port 8080
+	// Forcefully attaching socket to the port 8080
 	valread = bind(server_fd, (struct sockaddr *)&address, sizeof(address));
-    	if(valread < 0){
-        	perror("[-]Bind failed");
-        	exit(EXIT_FAILURE);
-    	}
+	if(valread < 0){
+	    perror("[-]Bind failed");
+	    exit(EXIT_FAILURE);
+	}
+
 	printf("[+]Bind to port %d\n", PORT);
 
-    	if(listen(server_fd, 10) == 0){
+	if(listen(server_fd, 10) == 0){
 		printf("[+]Listening....\n");
-	} else {
+	}
+
+	else {
 		perror("[-]Listen");
-        	exit(EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
 
 	while(1){
 		//int newsockfd = accept(server_fd,(struct sockaddr *) &address, &addr_size);
-		new_socket = accept(server_fd, (struct sockaddr *)&address,
-        				               	(socklen_t*)&addrlen);
+		new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
  		if(new_socket < 0){
 			exit(1);
 		}
@@ -113,50 +142,46 @@ int main(){
 		 */
 		printf("Connection accepted from %s:%d\n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
 
-
-		if((childpid = fork()) == 0){
-			close(server_fd);
+		//if((childpid = fork()) == 0){
+		//	close(server_fd);
 
 			while(1){
-				read(new_socket, buffer, 1024);
+				read(new_socket, buffer, BUFF_SIZE);
 
 				if(strcmp(buffer, ":exit") == 0){
 					printf("Disconnected from %s:%d\n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
 					break;
 				}
-        else{
 
-					while((token = strtok_r(p_buffer, " ", &p_buffer))){
-						printf("P_buffer %s\n", token);
+				else{
+				    //Split the intake args and make sure none have newline char or tab
+				    for(int i = 0; i < strlen(p_buffer); i++) {
+                        token[i] = strtok_r(p_buffer, " ", &p_buffer);
+                        int length = strlen(token[i]);
+                        if (token[i][length-1] == '\n' || token[i][length-1] == '\t')
+                            token[i][length-1]  = '\0';
+                    }
 
-            for(int i =0; i<strlen(break_down); i++){
-              break_down[i] = token;
-              printf("This is breakdown: %s : Len %d \n", break_down, strlen(break_down));
+					if (strcmp(token[0], "put") == 0) {
+					    receive_file(token[1], token[2], new_socket, inet_ntoa(address.sin_addr));
+					}
+
+					if (strcmp(token[0], "get") == 0) {
+					    printf("Get was found");
+					}
+
+					if (strcmp(token[0], "run") == 0) {
+					    printf("Run was found");
+					}
+
+					if (strcmp(token[0], "list") == 0) {
+					    printf("List was found");
+					}
+				}
+//			    printf("Client: %s\n", buffer);
+			    bzero(buffer, sizeof(buffer));
+			    send(new_socket, buffer, strlen(buffer), 0);
             }
-          }
-					if(strcmp(token, "put") == 0){
-						printf("Put was found");
-					}
-
-					if(strcmp(token, "get") == 0){
-						printf("Get was found");
-					}
-
-					if(strcmp(token, "run") == 0){
-						printf("Run was found");
-					}
-
-					if(strcmp(token, "list") == 0){
-						printf("List was found");
-					}
-        }
-			printf("Client: %s\n", buffer);
-			bzero(buffer, sizeof(buffer));
-			send(new_socket, buffer, strlen(buffer), 0);
-      }
-		}
+		//}
 	}
-	close(new_socket);
-
-  return 0;
 }
